2d88c43302ffe228a08d9221f134aa43f7212d05
dol-e <amorfati823@gmail.com>
2022-12-02 21:39:22 +0900

    props를 응용해서 상세페이지 만들기

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
 
   let [글제목, 글제목변경] = useState(['남자 코드 추천', '강남 우동 맛집', '파이썬 독학']);
   let [따봉, 따봉변경] = useState([0, 0, 0]);
-  // let [따봉, 따봉변경] = useState(0);
   let [modal, setModal] = useState(false);
+  let [title, setTitle] = useState(0);
 
   
 
@@ -19,22 +19,14 @@ function App() {
         <h4>React Blog</h4>
       </div>
 
-      {/* 
-      <div>
-        <h4 onClick = { ()=>{ setModal(!modal); } }>꾸꾸 분식</h4>
-        <p>11월 30일 발행</p>
-      </div> 
-      */}
-
       {
         글제목.map(function(a, i){
           return (
             <div className="list" key={i}>
-              {/* <h4 onClick = {()=>{ setModal(!modal)}}>{글제목[i]} </h4> */}
-              <h4>{글제목[i]} <span onClick={()=> {
+              <h4 onClick = {()=>{ setModal(!modal); setTitle(i)}}>{글제목[i]} <span onClick={()=> {
                 let copy = [...따봉];
                 copy[i] = copy[i] + 1;
-                따봉변경(copy)
+                따봉변경(copy);
                 }}>👍</span> {따봉[i]} </h4>
               <p>11월 30일 발행</p>
             </div>
@@ -42,23 +34,54 @@ function App() {
         })
       }
 
+      <input></input>
+
+
       {
-        modal == true ? <Modal/> : null // modal이 true면 <Modal/> 컴포넌트를 넣고 false면 아무것도 넣지 마라
+        modal == true ? <Modal 글제목={글제목} 글제목변경={글제목변경} title={title} /> : null
       }
 
     </div>
   );
 }
 
-function Modal() {
+/* 
+state를 자식에 만들면 부모 -> 자식 전송할 필요가 없다. 
+하지만 title이라는 state가 App에서도 필요하다면 App에 만들어놔야 한다. 
+state를 사용하는 컴포넌트들 중 최상위 컴포넌트에 만든다. 
+*/
+function Modal(props) {
   return (
     <div className="modal">
-      <h4>제목</h4>
+      <h4>{props.글제목[props.title]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button onClick={()=> {
+        let copy = [...props.글제목];
+        copy[0] = '여자 코드 추천';
+        props.글제목변경(copy);
+      }}>글수정</button>
     </div>
   )
 }
+
+
+/*
+function Modal(props) {
+  return (
+    <div className="modal">
+      <h4>{props.글제목[0]}</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+      <button onClick={()=> {
+        let copy = [...props.글제목];
+        copy[0] = '여자 코드 추천';
+        props.글제목변경(copy);
+      }}>글수정</button>
+    </div>
+  )
+}
+*/
 
 
 export default App;
